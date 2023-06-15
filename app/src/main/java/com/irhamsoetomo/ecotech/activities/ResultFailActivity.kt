9e7b6b8b93862021f54427ec033ff7ee10834156
irham <irhamsohan86@gmail.com>
2023-06-15 19:27:58 +0700

    - Adding result fail page - Adding title sign in page and register page

---
+++ app/src/main/java/com/irhamsoetomo/ecotech/activities/ResultFailActivity.kt
@@ -0,0 +1,12 @@
+package com.irhamsoetomo.ecotech.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.irhamsoetomo.ecotech.R
+
+class ResultFailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_fail)
+    }
+}
